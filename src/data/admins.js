083051cd049d66d3dd06a44e87f950de6db1583d
goldno <38741884+goldno@users.noreply.github.com>
2020-11-16 21:54:59 -0500

    Update admins.js

--- src/data/admins.js
+++ src/data/admins.js
@@ -2,7 +2,8 @@ import User from "../models/User.js";
 
 const admins = {
   '86648389457956864': new User('86648389457956864', 'Parker'),
-  '84810872899244032': new User('84810872899244032', 'Arjun')
+  '84810872899244032': new User('84810872899244032', 'Arjun'),
+  '90182872316510208': new User('90182872316510208', 'NoahPog')
 }
 
 export default admins
